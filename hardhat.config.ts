ddccbd3943e0fff5f3ae05369d0900becedaacb7
Ajay <ajaykumawat9904@gmail.com>
2024-12-25 16:59:13 +0530

    setup client side

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import { config as dotenvConfig } from "dotenv";
-
-dotenvConfig();
-
-const { SECRET_KEY, SEPOLIA_URL } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    sepolia: {
-      url: SEPOLIA_URL || "",
-      accounts: SECRET_KEY !== undefined ? [SECRET_KEY] : [],
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 1337,
     },
   },
 };
